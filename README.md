fb750f5eb97ae47c980bd95128e18777ab6c739d
Jose <jlopez.tics@gmail.com>
2020-06-04 11:40:06 -0400

    Login con Google

---
+++ README.md
@@ -0,0 +1,5 @@
+# Backend-Server
+
+Este es el código necesario para establecer el backend conectado a MongoDB usuando Mongoose.
+
+Para ejecutarlo: npm install
--- config/config.js
+++ config/config.js
@@ -1,1 +1,4 @@
 module.exports.SEED = 'este-es-un-seed-dificil';
+
+//GOOGLE ID
+module.exports.CLIENT_ID = '362622637930-h32ianel8pd8j7mt54l0eokmlv59b4q6.apps.googleusercontent.com'
